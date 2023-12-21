0ea9600bccd9df8046f6b4ed212a7fbb0fd18c58
Nathan Coxson <nathancoxson2739@gmail.com>
2023-12-21 07:05:44 -0500

    removed unused useCallback import

--- app/snake/GameBoard.tsx
+++ app/snake/GameBoard.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export default function GameBoard() {
     enum Direction {
