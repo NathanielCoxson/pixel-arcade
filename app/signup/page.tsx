734b08548d3e111be3b676447b8b6c69fff4016f
Nathan Coxson <nathancoxson2739@gmail.com>
2024-01-09 17:53:01 -0500

    added a signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,17 +1,19 @@
 'use client'
+
 import { useState } from "react";
 
-export default function Login() {
+export default function Signup() {
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [retypedPassword, setRetypedPassword] = useState<string>('');
 
     function handleSubmit(event: any) {
-        console.log({username, password});
+        console.log({username, password, retypedPassword});
     }
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-2">
-            <h1 className="text-4xl font-bold">Login</h1>
+            <h1 className="text-4xl font-bold">Signup</h1>
             <div className="flex flex-col gap-1">
                 <div className="flex w-full justify-between gap-2">
                     <label htmlFor="username">Username:</label>
@@ -29,6 +31,15 @@ export default function Login() {
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
+                    ></input>
+                </div>
+                <div className="flex min-w-full justify-between gap-2">
+                    <label htmlFor="retypedPassword">Retype Password:</label>
+                    <input
+                        name="retypedPassword"
+                        type="password"
+                        value={retypedPassword}
+                        onChange={(e) => setRetypedPassword(e.target.value)}
                     ></input>
                 </div>
                 <button onClick={handleSubmit}>Submit</button>
