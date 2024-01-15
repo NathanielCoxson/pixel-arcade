8f1d54f54cf76816967b882dd34a0c485a6781a4
Nathan Coxson <nathancoxson2739@gmail.com>
2024-01-15 18:27:10 -0500

    added bcrypt and password hashing on user sign up

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,15 +1,20 @@
 'use server';
 import prisma from "./prisma";
+const bcrypt = require('bcrypt');
 
 export async function createUser(prevState: any, formData: FormData) {
+    const saltRounds = 12;
     const rawData = {
         username: String(formData.get('username')),
         password: String(formData.get('password')),
     }
+
+    const hashedPassword = await bcrypt.hash(rawData.password, saltRounds);  
+
     const user = await prisma.users.create({
         data: {
             username: rawData.username,
-            password: rawData.password,
+            password: hashedPassword,
         },
     });
 
