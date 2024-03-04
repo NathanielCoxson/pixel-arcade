90c52d7a9ba40a30f441ce14800591fd9117ccaf
Nathan Coxson <nathancoxson2739@gmail.com>
2024-03-03 21:13:31 -0500

    changed dashboard to use router parameter for username rather than session information

--- app/dashboard/[username]/page.tsx
+++ app/dashboard/[username]/page.tsx
@@ -9,19 +9,19 @@ export default async function Dashboard({ params }: { params: { username: string
     let minesweeperScores: MinesweeperScore[] = []; 
     let snakeScores: SnakeScore[] = []; 
 
-    if (session) {
-        minesweeperScores = await getMinesweeperScores(session.user?.id);
-        minesweeperScores = minesweeperScores.sort((a: MinesweeperScore, b: MinesweeperScore) => {
-            if (a.time <= b.time) return -1;
-            else return 1;
-        });
+    // Minesweeper information
+    minesweeperScores = await getMinesweeperScores(params.username);
+    minesweeperScores = minesweeperScores.sort((a: MinesweeperScore, b: MinesweeperScore) => {
+        if (a.time <= b.time) return -1;
+        else return 1;
+    });
 
-        snakeScores = await getSnakeScores(session.user?.id);
-        snakeScores = snakeScores.sort((a: SnakeScore, b: SnakeScore) => {
-            if (a.score >= b.score) return -1;
-            else return 1;
-        });
-    }
+    // Snake information
+    snakeScores = await getSnakeScores(params.username);
+    snakeScores = snakeScores.sort((a: SnakeScore, b: SnakeScore) => {
+        if (a.score >= b.score) return -1;
+        else return 1;
+    });
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-center text-center p-24 gap-4">
