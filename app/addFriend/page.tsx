47ac0be6d9df001e8fca7c5e3beeeab8ebc16ab5
Nathan Coxson <nathancoxson2739@gmail.com>
2024-03-04 18:54:48 -0500

    feat: allow the user to search for another user to add

--- app/addFriend/page.tsx
+++ app/addFriend/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { getUsersWhereUsernameStartsWith } from "@/src/lib/actions";
 import { SyntheticEvent, useState } from "react";
+import Link from "next/link";
+
 export default function AddFriend() {
     const [searchTerm, setSearchTerm] = useState<string>("");
     const [searchResults, setSearchResults] = useState<string[]>([]);
@@ -13,17 +15,35 @@ export default function AddFriend() {
         console.log(result);
     }
 
+    //async function handleAddFriend(e: SyntheticEvent) {
+    //    e.preventDefault();
+
+    //}
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-center text-center p-24 gap-4">
             <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-                <label htmlFor="search">Search for user:</label>
-                <input name="search" onChange={(e) => setSearchTerm(e.target.value)}></input>
+                <label htmlFor="search">Search for a user:</label>
+                <input
+                    name="search" 
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    placeholder="Username"
+                    className="p-1"
+                ></input>
                 <button type="submit">Search</button>
             </form>
 
-            <ul>
-                {searchResults.map((result: string) => <li key={result}>{result}</li>)}
-            </ul>
+
+            {searchResults.length > 0 && <ul>
+                {searchResults.map((result: string) => <li key={result} className="flex gap-4">
+                        <Link 
+                            href={`/dashboard/${result}`}
+                            className="hover:font-bold transition-all"
+                        >
+                            {result}
+                        </Link>
+                </li>)}
+            </ul>}
 
         </main>
     )
